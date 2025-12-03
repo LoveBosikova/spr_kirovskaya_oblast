8d21f375d80f706d91a9454ee4a0975d65b49875
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2025-12-03 21:22:55 +0300

    news

--- src/components/bloks/News/News.tsx
+++ src/components/bloks/News/News.tsx
@@ -2,23 +2,32 @@ import VKPostEmbed from '../VKPostEmbed/VKPostEmbed';
 import style from './News.module.scss';
 
 export const NEWS_LINK = [
+  { 
+    id: "112265186_22158", 
+    hash: "pkuLkWQse2tkUrAErIJmhFkpM71v" 
+  },
+  { 
+    id: "112265186_22152", 
+    hash: "q3S9smxvLtJaAAxxJPZN-CCTDuYx" 
+  },
+  { 
+    id: "112265186_21988", 
+    hash: "-Dlj-anFIrEF6DmoZ5yrovxJ3AqM" 
+  },
     { 
-    id: "112265186_22079", 
-    hash: "eNoWWUvRLm7MWJm1glEYmqfWiHuR" 
-  },
-  { 
-    id: "212965583_6029", 
-    hash: "jd-gibyp6cdxGFYc8kyNub5MRDOh" 
-  },
-  { 
     id: "212965583_5992", 
     hash: "5gba8Mm2ReKl8erIbraOnH_z1Ppv"
   },
     { 
-    id: "112265186_21988", 
-    hash: "-Dlj-anFIrEF6DmoZ5yrovxJ3AqM" 
+    id: "212965583_6029", 
+    hash: "jd-gibyp6cdxGFYc8kyNub5MRDOh" 
+  },
+    { 
+    id: "112265186_22079", 
+    hash: "eNoWWUvRLm7MWJm1glEYmqfWiHuR" 
   },
 ];
+
 
 
 function News() {
